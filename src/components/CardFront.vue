480c3ea762eaed13fd6ba103b8bca39261a15664
protonio-gitH <55809448+protonio-gitH@users.noreply.github.com>
2023-08-18 03:56:03 +0300

    Update CardFront.vue

--- src/components/CardFront.vue
+++ src/components/CardFront.vue
@@ -3,14 +3,10 @@
   <div  class="card-front">  
     <div class="front">
       <div class="card-mark"><img src="@/img/chip.png"></div>
-      
       <div class="card-logo"><img :src="cardType" v-if="cardType"  alt=""></div>
-      
-      
       <div class="card-number">
         <label for="cardNumber" class="card-number__number">
           <span v-for="(n,$index) in defaultPlaceholder" :key="$index">
-            
             <div
               class="card-item__numberItem"
                   :class="{ '-active' : n.trim() === '' }"
@@ -23,45 +19,35 @@
                   v-else
                   :key="defaultPlaceholder + 1">{{ n}}</div>
           </span>
-        
         </label>
       </div>
       <div class="card-item__content">
       <div class="cardHolder">
-        
         <label for="cardName" class="card__info">
-          
           <div class="card__holder">Card Holder</div>
-          
           <div 
           class="card__name"
           v-if="FullName.length >0"
           >{{ FullName }}</div>
-        
           <div v-else class="card__name">Full Name</div>
       </label>
-    
       </div>
       <div class="card-expries">
         <label for="cardMonth" class="card-item__dateTitle">Expires</label>
         <label for="cardMonth" class="card-item__dateItem" >
-          
           <span :key="Month" v-if="Month">{{ Month }}</span>
           <span v-else >MM</span>
           </label>
           /
         <label for="cardYear" class="card-item__dateItem">
-        
           <span :key="Year" v-if="Year">{{ Year.slice(2,4) }}</span>
           <span v-else>YY</span>
-        
       </label>
     </div>
-    
   </div>
 </div>
-
 </div>
+
 <div  class="card-back" >
     <div class="card-band"></div>
     <div class="card-cvv">
@@ -73,8 +59,6 @@
     <div class="card-type"><img :src="cardType"></div>
 </div>
 </div>
-
-    
 </template>
 
 <script>
